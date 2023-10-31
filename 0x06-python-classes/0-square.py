f860e177a3753791bfa64c0b64058185b6010d41
Oyango Ondigo <138160676+ondi20@users.noreply.github.com>
2023-10-31 18:59:02 +0000

    update o-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
-class Square:
+class square:
+"""
+    This class defines a square.
 
+    A square is a four-sided polygon with all sides of equal length and all angles of 90 degrees.
+    It is a special case of a rectangle and can be characterized by the length of its sides.
+
+    """
     pass
